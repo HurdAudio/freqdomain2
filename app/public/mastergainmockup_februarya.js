4cbd6b76a75a82adcca14d634623faaedb14ef98
Devin Hurd <hurdaudio@gmail.com>
2017-12-01 18:46:00 -0700

    master volume february a mockup

---
+++ app/public/mastergainmockup_februarya.js
@@ -0,0 +1,59 @@
+'use strict';
+let muteAll = false;
+
+function visualMuting() {
+  let speakerIcon = document.getElementById('speakerIcon');
+  let masterGainDisplay = document.getElementById('masterGainDisplay');
+  let displaySpan = document.getElementById('displaySpan');
+  let masterGainFace = document.getElementById('masterGainFace');
+  let muteNote = document.getElementById('muteNote');
+  let masterGainTop = document.getElementById('masterGainTop');
+  let masterGainInput = document.getElementById('masterGainInput');
+
+
+  if (muteAll) {
+    displaySpan.innerHTML = 'mute';
+    speakerIcon.setAttribute("style", "opacity: 0.3;");
+    masterGainDisplay.setAttribute("style", "background: #ff0000; background-color: -webkit-linear-gradient(135deg, #ff0000, #6FAAF0); background: -o-linear-gradient(135deg, #ff0000, #6FAAF0); background: -moz-linear-gradient(135deg, #ff0000, #6FAAF0); background: linear-gradient(135deg, #ff0000, #6FAAF0); color: transparent; box-shadow: -1px -1px 1px #26FF26, -2px -2px 1px #26FF26, -3px -3px 1px #26FF26, -4px -4px 1px #26FF26;");
+    masterGainFace.setAttribute("style", "box-shadow: -1px -1px 1px #26FF26, -2px -2px 1px #26FF26, -3px -3px 1px #26FF26");
+    muteNote.setAttribute("style", "text-shadow: -1px -1px 1px #26FF26;");
+    masterGainTop.setAttribute("style", "filter: hue-rotate(90deg);");
+    masterGainInput.setAttribute("style", "filter: hue-rotate(90deg);");
+    setTimeout(()=>{
+      if (muteAll) {
+        masterGainDisplay.setAttribute("style", "background-color: transparent; background: url(./img/february/240_F_131355372_M07WEabz06WTACSC1aS7S7K9iiJ4QHBc.jpg); background-size: 300%; color: transparent; box-shadow: -1px -1px 1px #ff0000, -2px -2px 1px #26FF26, -3px -3px 1px #26FF26, -4px -4px 1px #26FF26;");
+        masterGainFace.setAttribute("style", "box-shadow: -1px -1px 1px #ff0000, -2px -2px 1px #ff0000, -3px -3px 1px #ff0000;");
+        muteNote.setAttribute("style", "text-shadow: -1px -1px 1px #26FF26, -2px -2px 1px #26FF26;");
+        setTimeout(()=>{
+          if (muteAll) {
+            visualMuting();
+          }
+        }, 500);
+      }
+
+    }, 500);
+  } else {
+    displaySpan.innerHTML = '';
+    speakerIcon.setAttribute("style", "opacity: 0.8;");
+    masterGainDisplay.setAttribute("style", "background-color: transparent; background: url(./img/february/240_F_131355372_M07WEabz06WTACSC1aS7S7K9iiJ4QHBc.jpg); background-size: 300%; color: #000000; box-shadow: -1px -1px 1px #26FF26, -2px -2px 1px #26FF26, -3px -3px 1px #26FF26, -4px -4px 1px #26FF26;");
+    masterGainFace.setAttribute("style", "box-shadow: -1px -1px 1px #26FF26, -2px -2px 1px #26FF26, -3px -3px 1px #26FF26;");
+    muteNote.setAttribute("style", "text-shadow: -1px -1px 1px #26FF26, -2px -2px 1px #26FF26;");
+    masterGainTop.setAttribute("style", "filter: hue-rotate(0deg);");
+    masterGainInput.setAttribute("style", "filter: hue-rotate(0deg);");
+  }
+}
+
+
+window.onload = ()=>{
+  let masterMute = document.getElementById('masterMute');
+
+  masterMute.addEventListener('click', ()=>{
+    if (muteAll) {
+      muteAll = false;
+      visualMuting();
+    } else {
+      muteAll = true;
+      visualMuting();
+    }
+  });
+};
