b1a7cc1744555c8f0e0cf2abc4cc65d554b0221a
Devin Hurd <hurdaudio@gmail.com>
2017-12-22 18:20:13 -0700

    added forgotten modulators to dynamic compressor table

--- migrations/20171222105800_dynamic_compressors.js
+++ migrations/20171222105800_dynamic_compressors.js
@@ -5,10 +5,15 @@ exports.up = function(knex, Promise) {
     table.integer('user_id').notNullable().defaultTo(1).references('id').inTable('users').onDelete('CASCADE').index();
     table.string('name').notNullable().defaultTo('dynamic compressor');
     table.float('threshold').notNullable().defaultTo(-24.00);
+    table.json('threshold_modulator').defaultTo(null);
     table.float('knee').notNullable().defaultTo(30.00);
+    table.json('knee_modulator').defaultTo(null);
     table.float('ratio').notNullable().defaultTo(12.00);
+    table.json('ratio_modulator').defaultTo(null);
     table.float('attack', 5, 3).notNullable().defaultTo(0.003);
+    table.json('attack_modulator').defaultTo(null);
     table.float('release', 5, 3).notNullable().defaultTo(0.250);
+    table.json('release_modulator').defaultTo(null);
     table.json('input').defaultTo(null);
     table.json('output').defaultTo(null);
     table.timestamps(true, true);
