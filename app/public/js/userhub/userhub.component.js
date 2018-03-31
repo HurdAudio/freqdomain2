d9326df479491f22b5f975f3f6e48fcc6106049a
Devin Hurd <hurdaudio@gmail.com>
2018-03-30 18:28:53 -0600

    bandpass filter in January B skin

--- app/public/js/userhub/userhub.component.js
+++ app/public/js/userhub/userhub.component.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   var currentUserId = 0;
-  var newsArray = [ 'FreqDomain2 is currently in pre-production.', 'Currently working on: Band Pass Filter - module 11.', 'LAA 2 ARI 3 in the top of the 6th inning in Arizona.', 'Impeachment proceedings pick up momentum as the US Senate prepares for a vote', 'Frequency Hertz so good...', 'Welcome to FreqDomain2 HUB', 'Social media guru expires in 280 characters', 'Bloodbath ensues in Tunisia after clerics attempt coup.', 'This is FreqDomain2 Headline News - the only place for news that matters to freqs', '' ];
+  var newsArray = [ 'FreqDomain2 is currently in pre-production.', 'Currently working on: Weather Module - module 10.', 'LAA 2 ARI 3 in the top of the 6th inning in Arizona.', 'Impeachment proceedings pick up momentum as the US Senate prepares for a vote', 'Frequency Hertz so good...', 'Welcome to FreqDomain2 HUB', 'Social media guru expires in 280 characters', 'Bloodbath ensues in Tunisia after clerics attempt coup.', 'This is FreqDomain2 Headline News - the only place for news that matters to freqs', '' ];
 
   angular.module('app')
     .component('userhub', {
