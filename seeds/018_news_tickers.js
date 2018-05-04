6b0101cb1033061e988c5ed61fb384cd44607fd2
Devin Hurd <hurdaudio@gmail.com>
2018-05-04 16:14:02 -0600

    update crawl

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Gain Module (module 2). Stage 12 - March Consequence - March Skins added to Skins Table & class constructor created.',
+          headline: 'Currently working on: Master Volume (module 1). Stage 13 - April A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
