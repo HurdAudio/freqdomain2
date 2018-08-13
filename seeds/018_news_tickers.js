cd706fef3316c52bca2d2621d02825b8733c97cf
Devin Hurd <hurdaudio@gmail.com>
2018-08-12 18:33:24 -0600

    Dynamic Compressor - Stage 11 - March C Skin

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Dynamic Compressor (module 5). Stage 11 - March C Skin.',
+          headline: 'Currently working on: Test Tone (module 4). Stage 12 - March Consequence - March Skins added to Skins Table/Class Constructor.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
