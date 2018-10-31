4e144553009d882f4f795d1fa36646297f6e0072
Devin Hurd <hurdaudio@gmail.com>
2018-10-30 18:56:12 -0600

    delay node - stage 1 - initial design in january a skin

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Delay Module (module 7). Stage 1 - Initialization.',
+          headline: 'Currently working on: Peaking Filter (module 16). Stage 2 - January B Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
