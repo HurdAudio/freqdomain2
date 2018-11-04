ee0f443a942ef4d607dbb4e6645543a6037e099e
Devin Hurd <hurdaudio@gmail.com>
2018-11-03 18:22:07 -0600

    lowshelf filter - stage 6 - february b skin

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Low Shelf Filter (module 12). Stage 6 - February B Skin.',
+          headline: 'Currently working on: Band Pass Filter (module 11). Stage 7 - February C Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
