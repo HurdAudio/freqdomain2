3977babdafadf2298ffab609370bd2e497d6a477
Devin Hurd <hurdaudio@gmail.com>
2018-11-17 19:51:07 -0700

    oscillator - stage 15 - april c skin

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Oscillator (module 3). Stage 15 April C Skin.',
+          headline: 'Currently working on: Gain (module 2). Stage 16 April Consequence - April skins added/full rendering support.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
