b996bdd21c34337e84a4fcd8d2c8318daf6de3ca
Devin Hurd <hurdaudio@gmail.com>
2018-12-09 16:13:55 -0700

    update progress

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Highshelf Filter (module 15). Stage 4 - January Consequence - Table with migration/seeds/routes.',
+          headline: 'Currently working on: Airlines Module (module 14). Stage 5 - February A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
