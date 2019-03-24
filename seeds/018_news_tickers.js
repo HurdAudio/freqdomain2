2b5d38f85ba30612882140f1e5403692bfc1713c
Devin Hurd <hurdaudio@gmail.com>
2019-03-24 11:46:22 -0600

    master volume - stage 19 - may c skin

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Master Volume (module 1). Stage 19 - May C Skin.',
+          headline: 'Currently working on: Front End (module 0). Stage 20 - Front End work.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
