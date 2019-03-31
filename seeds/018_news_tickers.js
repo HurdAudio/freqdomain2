f77671c250b4d83eaa3f191bc5f445fff9dea866
Devin Hurd <hurdaudio@gmail.com>
2019-03-31 15:48:04 -0600

    re-work info page

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Front End (module 0). Stage 20 - Front End work.',
+          headline: 'Currently working on: Allpass Filter (module 20). Stage 1 - Initial design. January A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
