f63ecaf5608e1d4232842d50fca096d43e6d7994
Devin Hurd <hurdaudio@gmail.com>
2019-04-16 17:50:09 -0600

    peaking filter - stage 5 - february a skin

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Peaking Filter (module 16). Stage 5 February A Skin.',
+          headline: 'Currently working on: Highshelf Filter (module 15). Stage 6 February B Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
