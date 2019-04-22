d57ce3962fc4036d20cb196a91a001606efc4101
Devin Hurd <hurdaudio@gmail.com>
2019-04-21 19:41:41 -0600

    documentation

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Low Shelf Filter (module 12). Stage 9 - March A Skin.',
+          headline: 'Currently working on: Band Pass Filter (module 11). Stage 10 - March B Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
