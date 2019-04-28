bd9569f6f51e8cf5e2d8e621b609f86d21a5e73b
Devin Hurd <hurdaudio@gmail.com>
2019-04-28 12:56:00 -0600

    class constructor created

--- app/public/js/userhub/userhub.component.js
+++ app/public/js/userhub/userhub.component.js
@@ -574,22 +574,75 @@
         //   output: null
         // }, {});
 
-        let highpassFilter = new HighpassFilter({
-            id: 1,
-            user_id: 1,
-            name: 'high pass filter',
-            frequency: 110.000,
-            frequency_modulator: null,
-            detune: 0.00,
-            detune_modulator: null,
-            q: 0.0000,
-            q_modulator: null,
-            input: null,
-            output: null
+        // let highpassFilter = new HighpassFilter({
+        //     id: 1,
+        //     user_id: 1,
+        //     name: 'high pass filter',
+        //     frequency: 110.000,
+        //     frequency_modulator: null,
+        //     detune: 0.00,
+        //     detune_modulator: null,
+        //     q: 0.0000,
+        //     q_modulator: null,
+        //     input: null,
+        //     output: null
+        // }, {});
+
+        let envelopeGenerator = new EnvelopeGenerator({
+          name: 'envelope generator',
+          attack_start: 0.00,
+          attack_start_modulator: null,
+          attack_time_interval: 1,
+          attack_time_interval_modulator: null,
+          attack_end: 100.00,
+          attack_end_modulator: null,
+          attack_exponential: false,
+          attack_convex: false,
+          attack_slope: 4,
+          decay_on: true,
+          decays: {
+            "decays": [
+              {
+                "decay_time_interval": 1,
+                "decay_time_interval_modulator": null,
+                "decay_end": 80.00,
+                "decay_end_modulator": null,
+                "decay_exponential": false,
+                "decay_convex": false,
+                "decay_slope": 4
+              }
+            ]
+          },
+          sustain_on: false,
+          sustain_modulator: null,
+          post_sustain_on: false,
+          post_sustains: {
+            "post_sustains": [
+              {
+                "post_sustain_time_interval": 1,
+                "post_sustain_time_interval_modulator": null,
+                "post_sustain_end": 80.00,
+                "post_sustain_end_modulator": null,
+                "post_sustain_exponential": false,
+                "post_sustain_convex": false,
+                "post_sustain_slope": 4
+              }
+            ]
+          },
+          release_time_interval: 1,
+          release_time_interval_modulator: null,
+          release_end_value: 0.00,
+          release_end_value_modulator: null,
+          release_exponential: false,
+          release_convex: false,
+          release_slope: 4,
+          output: null,
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }, {});
 
 
-        console.log(highpassFilter);
+        console.log(envelopeGenerator);
 
 
 
