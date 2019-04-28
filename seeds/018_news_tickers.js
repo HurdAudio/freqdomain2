85c0f6307f1e8eb542306310e27a1bdbd8eaef90
Devin Hurd <hurdaudio@gmail.com>
2019-04-28 12:59:00 -0600

    documentation

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Envelope Generator (module 9). Stage 12 - March Consequence - March Skins added to skins table. Class constructor.',
+          headline: 'Currently working on: Highpass Filter (module 8). Stage 13 - April A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
