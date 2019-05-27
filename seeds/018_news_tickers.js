c9078a402a59cc455c649bb5582ddf3483e72bec
Devin Hurd <hurdaudio@gmail.com>
2019-05-26 18:16:30 -0600

    update progress

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Front End (module 0). Stage 21 - Front end work.',
+          headline: 'Currently working on: Convolution Module (module 21). Stage 1 - INITIALIZATION. January A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
