0a1efc2587889779473fd38ca7889e097ce59034
Devin Hurd <hurdaudio@gmail.com>
2019-09-14 20:40:40 -0600

    update progress

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Peaking Filter (module 16). Stage 8 - February Consequence - Skins Table. Add January and February Skins.',
+          headline: 'Currently working on: Highshelf Filter (module 15). Stage 9 - March A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
