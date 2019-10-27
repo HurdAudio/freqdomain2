f6be281d06d5d253b35a53bf9890895e34d1c2c6
Devin Hurd <hurdaudio@gmail.com>
2019-10-27 12:25:27 -0600

    update progress

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Front End (module 0). Stage 24 - Front end work.',
+          headline: 'Currently working on: Stereo Splitter (module 24). Stage 1 - Initialization in January A Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
