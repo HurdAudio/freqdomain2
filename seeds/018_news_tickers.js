3b9b8b3a1731622ed63cae1598702825c5572ba3
Devin Hurd <hurdaudio@gmail.com>
2019-10-27 18:10:43 -0600

    update progress

--- seeds/018_news_tickers.js
+++ seeds/018_news_tickers.js
@@ -21,7 +21,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 3,
-          headline: 'Currently working on: Stereo Merger (module 23). Stage 2 - January B Skin.',
+          headline: 'Currently working on: Attenuation/Amplifier (module 22). Stage 3 - January C Skin.',
           expired: false,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
